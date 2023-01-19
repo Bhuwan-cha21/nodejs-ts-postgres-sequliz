e5e9f32b49a7299361b1e3cecaa198b51f694541
Bhuwan Sharma <bhuwanschalise21@gmail.com>
2023-01-19 15:24:21 +0545

    random generated

--- src/controller/itemsController.ts
+++ src/controller/itemsController.ts
@@ -3,10 +3,17 @@ const app = express()
 import { Request, Response } from 'express';
 let bodyparser = require('body-parser')
 let Sequlize = require('../database')
+const crypto = require('crypto');
 app.use(bodyparser.json());
 export const addItem = async (req: Request, res: Response) =>{
-    const {itemcode,t_code,t_data} = req.body
-
+    const {itemcode,t_data} = req.body
+    function generateId(length :Number) {
+        return crypto.randomBytes(Math.ceil(length / 2))
+          .toString('hex') // convert to hexadecimal format
+          .slice(0, length);   // return required number of characters
+      }
+      
+      const t_code = (generateId(8));
       const result = await Sequlize.query("SELECT * from  insert_items(:itemcode,:t_code,:t_data)", {
         replacements: { itemcode: itemcode, t_code: t_code, t_data:t_data },
         type: Sequlize.QueryTypes.SELECT
@@ -32,6 +39,7 @@ export const getItemById = async (req: Request, res: Response) =>{
         replacements: { id : id }
     })
     res.send(results)
+  
  
 }
 export const getItems = async (req: Request, res: Response) =>{
